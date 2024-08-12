31d0ee96c0aacdb54f088a913c14a8e37393ae25
AWinkelman02 <awinkelman02@gmail.com>
2024-08-12 08:13:42 -0500

    Implementing home page components.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material'
+
+import './App.css'
+import ExerciseDetail from './Pages/ExerciseDetail.js';
+import Home from './Pages/Home.js';
+import NavigationBar from './components/NavigationBar.js';
+import Footer from './components/Footer.js';
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box width="400px" sx={{ width: { xl: '1488px' }}} m='auto' >
+        <NavigationBar/>
+        <Routes>
+            <Route path="/" element={ <Home/> } />
+            <Route path="/exercises/:id" element={ <ExerciseDetail/> } />
+        </Routes>
+        <Footer />
+    </Box>
   )
 }
 
